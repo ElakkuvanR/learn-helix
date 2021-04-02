6c27a8368fbec651e2e87d872657bd643ac18808
Elakkuvan Rajamani (Valtech) <elakkuvan.rajamani@valtech.com>
2021-04-02 14:54:31 +0200

    Unicorn, ORMs are defined

---
+++ src/Foundation/ORM/code/Controllers/GlassStandardController.cs
@@ -0,0 +1,13 @@
+﻿using Sitecore.XA.Foundation.Mvc.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Learn.Helix.Foundation.ORM.Controllers
+{
+    public class GlassStandardController : StandardController
+    {
+
+    }
+}
